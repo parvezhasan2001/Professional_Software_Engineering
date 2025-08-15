adb23991f330f19c36a1492204d01c2bb475b01d
parvezhasan2001 <parvezh919@gmail.com>
2025-08-16 09:45:45 +1200

    Week-3 Activity-2

---
+++ Week-3/activity-2.py
@@ -0,0 +1,20 @@
+class DataProcessing:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def count_words(self):
+        with open(self.filename, "r", encoding="utf-8") as file:
+            count = 0
+            for line in file:
+                words = line.strip().split()
+                count += len(words)
+            return count
+
+
+def main():
+    demoFile = DataProcessing("demo_file.txt")
+    print(f"total number of words in file: {demoFile.count_words()} words")
+
+
+if __name__ == "__main__":
+    main()

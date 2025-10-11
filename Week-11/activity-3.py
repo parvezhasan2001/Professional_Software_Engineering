87bc38deeb7419229d3f1f6766c787f0eb7cfa38
parvezhasan2001 <parvezh919@gmail.com>
2025-10-12 11:50:21 +1300

    update

--- Week-11/activity-3.py
+++ Week-11/activity-3.py
@@ -5,19 +5,68 @@ import unittest
 import doctest
 
 def add(a, b):
+
+    """Return the sum of a and b.
+    >>> add(2, 3)
+    5
+    >>> add(-1, 1)
+    0
+    >>> add(-1, -1)
+    -2
+    """
     return a + b
 def multiply(a, b):
+    """Return the product of a and b.
+    >>> multiply(2, 3)
+    6
+    >>> multiply(-1, 1)
+    -1
+    >>> multiply(-1, -1)
+    1
+    """
     return a * b
 
 def subtract(a, b):
+    """Return the difference of a and b.
+    >>> subtract(5, 3)
+    2
+    >>> subtract(-1, -1)
+    0
+    >>> subtract(-1, 1)
+    -2
+    """
     return a - b
 
 def divide(a, b):
+    """Return the quotient of a and b.
+    >>> divide(6, 3)
+    2.0
+    >>> divide(-6, -3)
+    2.0
+    >>> divide(-6, 3)
+    -2.0
+    >>> divide(1, 0)
+    Traceback (most recent call last):
+        ...
+    ValueError: Cannot divide by zero
+    """
+
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
 
 def modulus(a, b):
+
+    """Return the modulus of a and b.
+    >>> modulus(5, 3)
+    2
+    >>> modulus(5, -3)
+    -1
+    >>> modulus(-5, 3)
+    1
+    >>> modulus(-5, -3)
+    -2
+    """
     return a % b
 
 class TestMathOperations(unittest.TestCase):
